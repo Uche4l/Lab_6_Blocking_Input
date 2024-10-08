bbaa63bd28f6e4413b9f063a734d8a782ee3c34e
samue <mbasu@mail.uc.edu>
2024-10-08 18:02:12 -0400

    init commit

--- src/HighOrLow.java
+++ src/HighOrLow.java
@@ -1,7 +1,72 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighOrLow
 {
     public static void main(String[] args)
     {
+        Random generator = new Random();
 
+        int targetNum = generator.nextInt(10) + 1;
+
+        Scanner in = new Scanner(System.in);
+
+        int guess = 0;
+
+        boolean valid = true;
+
+        String trash = "";
+
+        do{
+            System.out.print("Guess a number 1-10: ");
+
+            if (in.hasNextInt() ){
+
+                guess = in.nextInt();
+
+                if (guess >= 1 && guess <= 10) {
+
+                    int result = Integer.compare(guess, targetNum); // returns a switch case friendly value
+
+                    switch (result) {
+
+                        case 0:
+
+                            System.out.println("Correct! You've guessed the number!");
+
+                            break;
+
+                        case 1:
+
+                            System.out.println("Too high! Try again.");
+
+                            break;
+
+                        case -1:
+
+                            System.out.println("Too low! Try again.");
+
+                            break;
+                    }
+
+                    valid = true;
+
+                }else{
+
+                    System.out.println(""+guess+""+" is not a valid input");
+
+                    valid = false;
+                }
+
+            }else{
+
+                trash = in.next();
+
+                System.out.println(""+trash+""+" is not a valid input");
+
+                valid = false;
+            }
+
+        }while(!valid);
     }
 }
--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,7 +1,51 @@
+import java.util.Scanner;
+
 public class RectangleInfo
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        double height = 0;
+        double width = 0;
+        double area = 0;
+        double perimeter = 0;
+        double diagonal = 0;
+        String trash = "";
+        boolean doneWidth = false;
+        boolean doneHeight = false;
 
+        do {
+            System.out.print("Enter length of your rectangle: ");
+            if (in.hasNextDouble()){
+                height = in.nextDouble();
+                in.nextLine();
+                doneWidth = true;
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println(trash + " is not a valid length");
+            }
+        } while(!doneWidth);
+
+
+
+        do {
+            System.out.print("Enter width of your rectangle: ");
+            if (in.hasNextDouble()){
+                width = in.nextDouble();
+                doneHeight = true;
+            }else{
+                trash = in.nextLine();
+                System.out.println(trash + " is not a valid width");
+
+            }
+        }while(!doneHeight);
+
+        area = height * width;
+        System.out.println("The area of the rectangle is " + area + "ft squared");
+        perimeter = (2 * height) + (2 * width);
+        System.out.println("The perimeter of the rectangle is " + perimeter + "ft");
+        diagonal = Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
+        System.out.println("The diagonal of the rectangle is " + diagonal + "ft");
     }
 }
